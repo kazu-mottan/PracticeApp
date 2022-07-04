97682e62acd453acef2fafa07dc0f26e0a8da4b5
Kazuki.Sakamoto <kaitoxtennis1997mail@gmail.com>
2022-07-04 22:52:06 +0900

    init

---
+++ src/components/App.vue
@@ -0,0 +1,30 @@
+
+<script>
+import MainTitle from './parts/MainTitle.vue'
+export default {
+  name: 'App',
+  components: {
+    MainTitle
+  }
+}
+</script>
+
+<template>
+  <div> 
+    <MainTitle />
+  </div>
+</template>
+
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/parts/MainTitle.vue
@@ -0,0 +1,10 @@
+<template>
+      <h1>unTitle</h1>
+
+</template>
+
+<script>
+export default {
+  name: 'MainTitle'
+}
+</script>
