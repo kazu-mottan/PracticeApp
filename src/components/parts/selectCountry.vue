9025d984251e63a43493c4eb5e1d970ab8f41d47
Masahiro Tomonaga <Masahiro.Tomonaga@ibm.comgit config --global user.email Masahiro.Tomonaga@ibm.com>
2022-07-25 20:39:25 +0900

    20220725_tomonaga

---
+++ src/components/parts/selectCountry.vue
@@ -0,0 +1,15 @@
+<template>  
+    <v-select
+        :items="items"
+        label="Select your country"
+    ></v-select>
+</template>
+
+<script>
+export default {
+    name: 'selectCountry',
+    data: () => ({
+        items: ['Japan', 'United States', 'United Kingdom', 'China', 'Taiwan', 'Spain', 'Germany', 'Canada'],
+    }),
+}
+</script>
