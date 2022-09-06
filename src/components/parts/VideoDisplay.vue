8f48dec7775d37d19757cef80cfbeb522abf44e3
Masahiro Tomonaga <Masahiro.Tomonaga@ibm.comgit config --global user.email Masahiro.Tomonaga@ibm.com>
2022-09-06 20:37:50 +0900

    0906

---
+++ src/components/parts/VideoDisplay.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <iframe
+            width="1200" 
+            height="700"
+            src="https://www.youtube.com/embed/I4ehUn3582o"
+        ></iframe>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'VideoDisplay'
+    }
+</script>
