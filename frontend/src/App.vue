62cbf17c60661b827fd48a56e12321fe839795f0
shibm760corp <shuko.hirano@ibm.com>
2022-10-01 13:01:23 +0900

    フッターとヘッダー

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,35 +1,39 @@
 <template>
   <v-app>
-    <v-main>
-      <router-view />
-    </v-main>
+    <AppHeader />
+      <v-main>
+        <router-view />
+      </v-main>
+    <AppFooter />
   </v-app>
 </template>
 
 <script>
-import MainTitle from './components/parts/MainTitle.vue'
-import SelectMovie from './components/parts/SelectMovie.vue'
-import UserPassField from './components/parts/UserPassField.vue'
-import UserTextField from './components/parts/UserTextField.vue'
-import SelectCountry from './components/parts/SelectCountry.vue'
-import SelectLanguage from './components/parts/SelectLanguage.vue'
-import SubmitButton from './components/parts/SubmitButton.vue'
+import AppFooter from "./components/parts/AppFooter.vue";
+import AppHeader from "./components/parts/AppHeader.vue";
+import MainTitle from "./components/parts/MainTitle.vue";
+import SelectMovie from "./components/parts/SelectMovie.vue";
+import UserPassField from "./components/parts/UserPassField.vue";
+import UserTextField from "./components/parts/UserTextField.vue";
+import SelectCountry from "./components/parts/SelectCountry.vue";
+import SelectLanguage from "./components/parts/SelectLanguage.vue";
+import SubmitButton from "./components/parts/SubmitButton.vue";
 
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
+    AppFooter,
+    AppHeader,
     MainTitle,
     SelectMovie,
     UserTextField,
     UserPassField,
     SelectCountry,
     SelectLanguage,
-    SubmitButton
+    SubmitButton,
   },
 
-  data: () => ({
-    
-  }),
-}
+  data: () => ({}),
+};
 </script>
