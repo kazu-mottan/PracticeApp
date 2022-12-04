63875d3a2fb2bbb4b0416409dd1808e7cecba5a3
Kazuki.Sakamoto <kaitoxtennis1997mail@gmail.com>
2022-12-04 21:52:58 +0900

    backend周り修正

--- backend/mysql/init/user.sql
+++ backend/mysql/init/user.sql
@@ -3,10 +3,9 @@ DROP TABLE IF EXISTS deetube.userdb;
 CREATE TABLE deetube.userdb(
     id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     username VARCHAR(50),
-    userid VARCHAR(50),
     userpass  VARCHAR(50),
     country TEXT,
     lang TEXT,
     genre TEXT
 );
-INSERT INTO deetube.userdb(username,userid,userpass,country,lang,genre) VALUES ('Kazuki','test','test','JP','JP','sports');
+INSERT INTO deetube.userdb(username,userpass,country,lang,genre) VALUES ('Kazuki','test','test','JP','JP','sports');
